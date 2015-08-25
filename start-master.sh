6a94759d5f5f48f3e06bebc5c68ef1d035783c5b
EC2 Default User <ec2-user@ip-10-0-3-101.us-west-2.compute.internal>
2015-08-25 09:46:53 +0000

    Debugging the failure

--- start-master.sh
+++ start-master.sh
@@ -3,4 +3,6 @@ echo "Starting Ambari Server..."
 ambari-server setup -j /usr --database=postgres  --databasehost=localhost --databaseport=5432 --databasename=ambari  --postgresschema=ambarischema --databaseusername=ambari --databasepassword=bigdata  --silent
 rm -fr /var/log/hadoop  /var/run/hadoop
 ambari-server start
-/bin/bash
+cat /var/log/ambari-server/ambari-server.out
+cat /var/log/ambari-server/ambari-server.log
+while [ true ];  do  sleep 5 ; done
