c79d32481d450233879b5ca20fbec09f6f90f212
Senthil <senthil69@yahoo.com>
2015-09-21 11:05:26 +0000

    Fixed syntax error in hdp.repo

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ sleep 3
 sudo docker build -f ambari-agent.Dockerfile -t ambari-agent:1 .
 sleep 3
 sudo docker build -f ambari-client.Dockerfile -t ambari-client:1 .
+sleep 3
+sudo docker build -f ambari-repo.Dockerfile -t ambari-repo:1 .
+sleep 3
+sudo docker build -f docker-repo.Dockerfile -t docker-repo:1 .
