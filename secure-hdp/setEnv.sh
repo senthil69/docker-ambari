a72bd59e97039c24b915ceabec950b91723d6c4a
Shanmugam Senthil <senthil69@yahoo.com>
2016-02-17 06:58:37 +0000

    Changed root suffix

--- secure-hdp/setEnv.sh
+++ secure-hdp/setEnv.sh
@@ -1,7 +1,7 @@
 
-export BASE_DN="dc=us-west-2,dc=compute,dc=internal"
-export ROOT_DN="cn=Manager,dc=us-west-2,dc=compute,dc=internal"
+export BASE_DN="dc=sds,dc=com"
+export ROOT_DN="cn=Manager,dc=sds,dc=com"
 export HOST_FQDN=`hostname -f`
 export HOST_KRB5=`hostname -f`
-export KRB5_DOMAIN=US-WEST-2.COMPUTE.INTERNAL
+export KRB5_DOMAIN=SDS.COM
 export HOST_DOMAIN=us-west-2.compute.internal
