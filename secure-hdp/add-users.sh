fc436f356901fdef8b1bfee7de9ec083bdc36d81
Shanmugam Senthil <senthil69@yahoo.com>
2016-02-17 07:57:39 +0000

    two org user setup

--- secure-hdp/add-users.sh
+++ secure-hdp/add-users.sh
@@ -4,15 +4,15 @@
 
 ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "uid=john,ou=People,$BASE_DN"
 ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "uid=david,ou=People,$BASE_DN"
-ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "cn=miners,ou=PosixGroups,$BASE_DN"
-ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "cn=miners,ou=Groups,$BASE_DN"
+ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "cn=cokebiz,ou=PosixGroups,$BASE_DN"
+ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "cn=cokebiz,ou=Groups,$BASE_DN"
 ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "ou=PosixGroups,$BASE_DN"
 ldapdelete -x -D cn=Manager,$BASE_DN -w admin123  "ou=Groups,$BASE_DN"
 
 
 
-export JPWD=`slappasswd -s john`
-export DPWD=`slappasswd -s david`
+export JPWD=`slappasswd -s sds`
+export DPWD=`slappasswd -s coke`
 
 cat <<EOF >addusers.ldif 
 
@@ -24,10 +24,22 @@ dn: ou=PosixGroups,$BASE_DN
 objectClass: organizationalUnit
 ou: Group
 
-dn: cn=miners,ou=PosixGroups,$BASE_DN
+dn: cn=cokebiz,ou=PosixGroups,$BASE_DN
 objectClass: posixGroup
-cn: miners
+cn: cokebiz
 gidNumber: 5000
+dn: cn=cokedev,ou=PosixGroups,$BASE_DN
+objectClass: posixGroup
+cn: cokedev
+gidNumber: 5001
+dn: cn=sdsbiz,ou=PosixGroups,$BASE_DN
+objectClass: posixGroup
+cn: sdsbiz
+gidNumber: 5002
+dn: cn=sdsdev,ou=PosixGroups,$BASE_DN
+objectClass: posixGroup
+cn: sdsdev
+gidNumber: 5003
 
 
 dn: uid=john,ou=People,$BASE_DN
@@ -43,7 +55,7 @@ cn: John Doe
 displayName: John Doe
 uidNumber: 10000
 gidNumber: 5000
-userPassword: $JPWD
+userPassword: $DPWD
 gecos: John Doe
 loginShell: /bin/bash
 homeDirectory: /home/john
@@ -66,12 +78,143 @@ gecos: David Kim
 loginShell: /bin/bash
 homeDirectory: /home/david
 
-dn: cn=miners,ou=Groups,$BASE_DN
+dn: uid=jane,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: jane
+sn: Jane
+givenName: Ann
+cn: Jane Ann
+displayName: Jane Ann
+uidNumber: 10002
+gidNumber: 5001
+userPassword: $DPWD
+gecos: Jane Ann
+loginShell: /bin/bash
+homeDirectory: /home/jane
+
+dn: uid=lucy,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: lucy
+sn: Lucy
+givenName: Morning
+cn: Lucy Morning
+displayName: Lucy Morning
+uidNumber: 10003
+gidNumber: 5001
+userPassword: $DPWD
+gecos: Lucy Morning
+loginShell: /bin/bash
+homeDirectory: /home/lucy
+
+
+dn: uid=jkim,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: jkim
+sn: Kim
+givenName: Jong Ho
+cn: Jong Ho Kim
+displayName: Jong Ho Kim
+uidNumber: 10004
+gidNumber: 5002
+userPassword: $JPWD
+gecos: Jong Ho Kim
+loginShell: /bin/bash
+homeDirectory: /home/jkim
+
+
+dn: uid=jjung,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: jjung
+sn: Jung
+givenName: Jaewoon
+cn: Jaewoon Jung
+displayName: Jaewoon Jung
+uidNumber: 10005
+gidNumber: 5002
+userPassword: $JPWD
+gecos: Jaewoon Jung
+loginShell: /bin/bash
+homeDirectory: /home/jjung
+
+
+dn: uid=jpark,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: jpark
+sn: Park
+givenName: Joseph
+cn: Joseph Park
+displayName: Joseph Park
+uidNumber: 10006
+gidNumber: 5003
+userPassword: $JPWD
+gecos: Jospeph Park
+loginShell: /bin/bash
+homeDirectory: /home/jpark
+
+
+dn: uid=jjoo,ou=People,$BASE_DN
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+objectClass: organizationalPerson
+objectClass: person
+uid: jjoo
+sn: Joo
+givenName: Jaemin
+cn: Jaemin Joo
+displayName: Jaemin Joo
+uidNumber: 10007
+gidNumber: 5003
+userPassword: $JPWD
+gecos: Jaemin Joo
+loginShell: /bin/bash
+homeDirectory: /home/jjoo
+
+dn: cn=cokebiz,ou=Groups,$BASE_DN
 objectClass: top
 objectClass: groupofnames
-cn: miners
+cn: cokebiz
 member: uid=john,ou=People,$BASE_DN
 member: uid=david,ou=People,$BASE_DN
+dn: cn=cokedev,ou=Groups,$BASE_DN
+objectClass: top
+objectClass: groupofnames
+cn: cokedev
+member: uid=jane,ou=People,$BASE_DN
+member: uid=lucy,ou=People,$BASE_DN
+
+dn: cn=sdsbiz,ou=Groups,$BASE_DN
+objectClass: top
+objectClass: groupofnames
+cn: sdsbiz
+member: uid=jkim,ou=People,$BASE_DN
+member: uid=jjung,ou=People,$BASE_DN
+dn: cn=sdsdev,ou=Groups,$BASE_DN
+objectClass: top
+objectClass: groupofnames
+cn: sdsdev
+member: uid=jpark,ou=People,$BASE_DN
+member: uid=jjoo,ou=People,$BASE_DN
 
 EOF
 
